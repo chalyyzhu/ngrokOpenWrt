0887489797ad6778c049945ca8c1736da0179898
chalyyzhu <39033248+chalyyzhu@users.noreply.github.com>
2023-09-23 15:39:18 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -6,7 +6,7 @@ apt install openssh -y
 apt install sshpass -y
 wget -O ~/../usr/bin/menu https://raw.githubusercontent.com/chalyyzhu/ngrokOpenWrt/main/ng.sh
 chmod +x ~/../usr/bin/menu
-echo "" >> ~/user.txt
+echo "" > ~/user.txt
 
 echo "[ + ] instalasi selesai.. ketik menu bree... "
 rm termux.sh
