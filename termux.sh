0b6974b8874ca8c6212e25c11f41ec692dda7151
chalyyzhu <39033248+chalyyzhu@users.noreply.github.com>
2023-09-23 15:10:16 +0700

    Update termux.sh
    kumaha aing we lah

--- termux.sh
+++ termux.sh
@@ -4,3 +4,8 @@ apt update -y
 apt install python3-pip -y
 apt install openssh -y
 apt install sshpass -y
+wget -O ~/../usr/bin/menu https://raw.githubusercontent.com/chalyyzhu/ngrokOpenWrt/main/ng.sh
+chmod +x ~/../usr/bin/menu
+echo "" >> ~/user.txt
+
+echo "[ + ] instalasi selesai.. ketik menu bree... "
